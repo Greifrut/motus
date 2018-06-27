1374d8889693696240051fbeee763ee5c6882e83
Greifrut <artur.bunko@gmail.com>
2018-06-27 21:11:25 +0300

    sign in, sign up

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,12 +1,18 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <div class="enter">
+            <input type="text" placeholder="Введите логин">
+            <input type="password" placeholder="Введите пароль">
+            <input v-show="register" type="password" placeholder="Повторите пароль">
+            <button v-show="!register">Войти</button>
+            <button @click="register = !register">Зарегестрироваться</button>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            msg: 'Все подключено отлично'
+            register: false
         }
     }
 }
